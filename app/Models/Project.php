27b652c3e50288d1f81081836a5e78f43d4c243e
kmillideveloper <kmilli.developer@gmail.com>
2021-11-26 11:26:21 +0500

    project generate activit

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -30,4 +30,9 @@ class Project extends Model
     {
         return $this->hasMany(Task::class);
     }
+
+    public function activity()
+    {
+        return $this->hasMany(Activity::class);
+    }
 }
