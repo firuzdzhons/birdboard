aa2167c9e43aae94ee652f359b3018e26a4c3c44
Firuzjon.Saidov <firuzdzhonsa@gmail.com>
2021-11-28 21:38:29 +0500

    view activity

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -62,9 +62,12 @@
             </div>
          
 
-            <div class="lg:w-1/4 px-3">
+            <div class="lg:w-1/4 px-3 lg:py-8">
                 @include('projects.card')
+                @include('projects.activity.card')
             </div>
+
+            
         </div>
     </main>
 </x-app-layout>
